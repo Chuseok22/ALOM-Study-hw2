2ed740ee4abb1ec7d2a8cce44eb4ec70d866b367
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-10-06 17:08:13 +0900

    [FEAT] Entity 작성

--- src/main/java/com/example/springhw2/entity/Post.java
+++ src/main/java/com/example/springhw2/entity/Post.java
@@ -1,4 +1,33 @@
 package com.example.springhw2.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
 public class Post {
+
+    // TODO : 글 제목, 본문, 작성자, 글 작성 시간, 글 수정 시간
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
 }
