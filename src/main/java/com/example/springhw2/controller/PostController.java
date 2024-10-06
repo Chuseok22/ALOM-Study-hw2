93fac5887c45c7e2b19a4b8600ef07f4db9933eb
Uechann <cab07036@naver.com>
2024-10-06 19:37:47 +0900

    hw2 Entity Repository Service Controller 완료

--- src/main/java/com/example/springhw2/controller/PostController.java
+++ src/main/java/com/example/springhw2/controller/PostController.java
@@ -1,4 +1,80 @@
 package com.example.springhw2.controller;
 
+import com.example.springhw2.entity.Post;
+import com.example.springhw2.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
 public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {this.postService = postService;}
+
+    //글 작성
+    @PostMapping("")
+    public ResponseEntity<Post> createPost(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content,
+            @RequestParam("writer") String writer) {
+        Post post = postService.createPost(title, content, writer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(post);
+        //201 Created 상태 정보와 Body를 같이 return
+    }
+
+    //모든 글 조회
+    @GetMapping("")
+    public ResponseEntity<List<Post>> getAllPosts() {
+        List<Post> posts = postService.getAllPosts();
+        return ResponseEntity.status(HttpStatus.OK).body(posts);
+        //200 ok 상태 반환
+    }
+
+    //Id를 통한 글 조회
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable("id") Long id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(post);
+    }
+
+    //title을 통한 조회
+    @GetMapping("/title/{title}")
+    public ResponseEntity<Post> getPostByTitle(@PathVariable("title") String title) {
+        Post post = postService.findByTitle(title);
+        return ResponseEntity.status(HttpStatus.OK).body(post);
+    }
+
+    //wrtier를 통한 조회
+    @GetMapping("/writer/{writer}")
+    public ResponseEntity<List<Post>> getPostByWriter(@PathVariable("writer") String writer) {
+        List<Post> posts = postService.findByWriter(writer);
+        return ResponseEntity.status(HttpStatus.OK).body(posts);
+    }
+
+    //제목, 내용 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(
+            @PathVariable("id") Long id,
+            @RequestParam("title") String title,
+            @RequestParam("content") String content) {
+
+        Post post = postService.editPost(id, title, content);
+        return ResponseEntity.status(HttpStatus.OK).body(post);
+    }
+
+    //글 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable("id") Long id) {
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+        //204 Not Found 상태 return
+    }
 }
--- src/main/java/com/example/springhw2/service/PostService.java
+++ src/main/java/com/example/springhw2/service/PostService.java
@@ -1,4 +1,85 @@
 package com.example.springhw2.service;
 
+import com.example.springhw2.entity.Post;
+import com.example.springhw2.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
 public class PostService {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    //CRUD
+    //Post Create
+    public Post createPost(String title, String content, String writer) {
+
+        //title또는 content가 null이면 예외처리
+        if(title == null || content == null || title.isEmpty()) {
+            throw new IllegalArgumentException("title and content cannot be null");
+        }
+
+        Post post = new Post();
+        post.setTitle(title);
+        post.setContent(content);
+        post.setWriter(writer);
+        post.setPostDate(LocalDateTime.now());
+        post.setEditedDate(LocalDateTime.now());
+
+        return postRepository.save(post);
+    }
+
+    //Post Read
+    //모든 글 조회
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    //id로 조회
+    public Post findById(Long id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Id에 해당하는 Post가 없습니다."));
+        //findById return 값이 Optional<Post> 이므로 Post 객체를 반환하기 위해서
+        //orElseThrow() 예외처리를 던어 로직에서 예외를 처리 하기 편하게 만듬
+    }
+
+    //title로 조회
+    public Post findByTitle(String title) {
+        return postRepository.findByTitle(title)
+                .orElseThrow(() -> new NoSuchElementException("Title에 해당하는 Post가 없습니다."));
+    }
+
+    //writer로 조회
+    public List<Post> findByWriter(String writer) {
+        return postRepository.findByWriter(writer);
+    }
+
+    //Post Update
+    //게시글 수정
+    //수정과 삭제 부분은 Transactional 어노테이션을 사용해서
+    //자동으로 롤백해주는 기능을 추가
+    @Transactional
+    public Post editPost(Long id, String title, String content) {
+        Post post = findById(id);
+        post.setTitle(title);
+        post.setContent(content);
+        post.setEditedDate(LocalDateTime.now());
+
+        return postRepository.save(post);
+    }
+
+    //Post Delete
+    @Transactional
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
+    }
 }
